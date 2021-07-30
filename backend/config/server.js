4e8d5edab287e3c35e2877bf03c26d5987ba7959
allanbraun <allanvobraun@gmail.com>
2021-07-30 18:01:19 -0300

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cfec38789031174646b260050d0bffe4'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '51aa24a1-46f1-40a5-82ce-2790d0828050'
+};
