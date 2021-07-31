8dabbddf9a81dd81f61376bfe22a0e3275138b6a
allanbraun <allanvobraun@gmail.com>
2021-07-31 16:16:47 -0300

    retirado transition

--- frontend/src/components/NavBar.svelte
+++ frontend/src/components/NavBar.svelte
@@ -6,7 +6,7 @@
     ref: string;
   }
 
-//   const items = [];
+  //   const items = [];
   const items: MenuItem[] = [
     {
       name: "Dashboard",
@@ -125,7 +125,7 @@
 
   <!-- Mobile menu, show/hide based on menu state. -->
   {#if mobileMenu == true}
-    <div class="sm:hidden" id="mobile-menu" transition:slide>
+    <div class="sm:hidden" id="mobile-menu" >
       <div class="px-2 pt-2 pb-3 space-y-1">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
         <a
