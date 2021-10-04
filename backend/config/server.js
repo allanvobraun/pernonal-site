1bdc92da5145131e36eb3213adc3840806676619
allanbraun <allanvobraun@gmail.com>
2021-10-04 17:04:28 -0300

    dominio hard coded por enquanto

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://allanbraun.net/strapi',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'cfec38789031174646b260050d0bffe4'),
