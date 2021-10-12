c8e41fdc2d6c46ffb8c4e17af7e3496fed18ca61
allanbraun <allanvobraun@gmail.com>
2021-10-12 12:14:31 -0300

    SEO para pagina do blog

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -8,10 +8,15 @@
 	import Footer from '$components/Footer.svelte';
 </script>
 
+<svelte:head>
+	<title>Homepage Allan Braun</title>
+	<meta name="description" content="Homepage do allan braun, desenvolvedor fullstack" />
+</svelte:head>	
+
 <header class="fixed top-0 z-50">
 	<NavBar />
 </header>
 <main class="flex flex-col">
 	<slot />
 </main>
-<Footer></Footer>
+<Footer />
